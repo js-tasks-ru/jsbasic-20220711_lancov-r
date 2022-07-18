f554f232d3c2d2a8a5513aa40874c36b3a01491a
Roman Lancov <skvirl@gmail.com>
2022-07-18 22:46:58 +0300

    3-module done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  return str.split(' ').reduce((obj,val)=>{
+
+    val = Number(val);
+    if( isNaN(val) ) return obj
+ 
+    obj.min = obj.min < val ? obj.min : val;
+    obj.max = obj.max > val ? obj.max : val;
+    
+    return obj;
+
+  },
+  {min: Infinity, max: -Infinity})
 }
