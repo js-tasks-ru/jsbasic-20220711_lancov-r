569de6840d17ed453e8a81dabb887645322ea35e
John Snow <skvirl@gmail.com>
2022-07-22 12:53:52 +0300

    4-module done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,14 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // for (const key in table.rows) {
+  //   console.log(key);
+  // }
+  let rowCounter = 0;
+  for (const row of table.rows) {
+    let cellCounter = 0;
+    for (const cell of row.cells) {
+      if (cellCounter === rowCounter) cell.style.backgroundColor = "red";
+      cellCounter++;
+    }
+    rowCounter++;
+  }
 }
