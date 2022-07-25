bbe259d0eea4b028b65e99edd2233d26eb4442d3
Roman Lancov <skvirl@gmail.com>
2022-07-25 22:44:34 +0300

    5-module almost done...

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,52 +1,47 @@
 function initCarousel() {
-  const carouselArrowRight = document.getElementsByClassName(
-    "carousel__arrow_right"
-  )[0];
-  const carouselArrowLeft = document.getElementsByClassName(
-    "carousel__arrow_left"
-  )[0];
 
-  const carousel__inner = document.getElementsByClassName("carousel__inner")[0];
 
-  carousel__inner.dataset.currentScrollState = 0;
+  const carousel = document.querySelector(".carousel");
+  const carouselArrowRight = document.querySelector(".carousel__arrow_right");
+  const carouselArrowLeft  = document.querySelector(".carousel__arrow_left");
+
+  carouselArrowLeft.style.display = "none";
 
   carouselArrowRight.addEventListener("click", carouselScrolling);
   carouselArrowRight.addEventListener("click", carouselArrowButtonVisibility);
 
   carouselArrowLeft.addEventListener("click", carouselScrolling);
   carouselArrowLeft.addEventListener("click", carouselArrowButtonVisibility);
+
 }
 
 function carouselScrolling(event) {
-  const carousel__inner = document.getElementsByClassName("carousel__inner")[0];
-  let currentScrollState = Number(carousel__inner.dataset.currentScrollState);
+  const carousel = event.currentTarget.closest(".carousel");
+  let currentScrollState = parseInt(carousel.dataset.currentScrollState,10) || 0;
 
-  if (event.target.classList.contains("carousel__arrow_right"))
+  if (event.currentTarget.classList.contains("carousel__arrow_right"))
     currentScrollState--;
-  if (event.target.classList.contains("carousel__arrow_left"))
+  if (event.currentTarget.classList.contains("carousel__arrow_left"))
     currentScrollState++;
 
-  event.target.classList.contains("carousel__arrow_right");
-
-  carousel__inner.dataset.currentScrollState = currentScrollState;
-
-  const elementWidth = 985.45;
-  const currentOffset = elementWidth * currentScrollState;
-  carousel__inner.style.transform = `translateX(${currentOffset}px)`;
+  carousel.dataset.currentScrollState = currentScrollState;
+  console.log(carousel.dataset.currentScrollState);
+  carousel.querySelector(
+    ".carousel__inner"
+  ).style.transform = `translateX(${ 100 * currentScrollState}%)`;
 }
+
 function carouselArrowButtonVisibility(event) {
-  const carousel__inner = document.getElementsByClassName("carousel__inner")[0];
+  const carousel = event.currentTarget.closest(".carousel");
+  const carouselArrowRight = carousel.querySelector(".carousel__arrow_right");
+  const carouselArrowLeft = carousel.querySelector(".carousel__arrow_left");
 
-  const carouselArrowRight = document.getElementsByClassName(
-    "carousel__arrow_right"
-  )[0];
-  const carouselArrowLeft = document.getElementsByClassName(
-    "carousel__arrow_left"
-  )[0];
+  const currentScrollState = carousel.dataset.currentScrollState;
+  const carouselChildrenElementsCount =
+    carousel.querySelector(".carousel__inner").childElementCount;
 
-  carouselArrowRight.hidden =
-    carousel__inner.dataset.currentScrollState ===
-    carousel__inner.childElementCount - 1;
-
-  carouselArrowLeft.hidden = carousel__inner.dataset.currentScrollState === 0;
+  carouselArrowLeft.style.display =
+    currentScrollState === "0" ? "none" : "";
+  carouselArrowRight.style.display =
+    -currentScrollState === carouselChildrenElementsCount - 1 ? "none" : "";
 }
