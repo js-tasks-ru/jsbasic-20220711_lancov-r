780daacb1912e98e19543380ae3d7514ea2c2e75
John Snow <skvirl@gmail.com>
2022-08-12 13:07:39 +0300

    8-2 in progress...

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,64 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.#render();
+  }
+
+  #filters = {};
+
+  set filters(val) {
+    for (const key in val) this.#filters[key] = val[key];
+  }
+
+  // productGrid.updateFilter({ noNuts: event.target.checked });
+  // productGrid.updateFilter({ vegeterianOnly: event.target.checked });
+  // productGrid.updateFilter({ maxSpiciness: 2 });
+  // productGrid.updateFilter({ maxSpiciness: 4 });
+  // productGrid.updateFilter({ category: 'soups' });
+  // productGrid.updateFilter({ category: '' });
+
+  #render() {
+    this.elem = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+        ${this.products
+          .map(this.#eligibledProduct, productObj)
+          .reduce(
+            (renderedCards, card) =>
+              (renderedCards += card === null ? "" : card.outerHTML),
+            ""
+          )}
+      </div>
+    </div>
+    `);
+  }
+
+  #eligibledProduct(productObj) {
+    const filters = this.#filters;
+
+    if (filters.noNuts && "nuts" in productObj) return null;
+    if (filters.vegeterianOnly && !"vegeterian" in productObj) return null;
+    if (
+      "maxSpiciness" in filters &&
+      filters.maxSpiciness < productObj.spiciness
+    )
+      return null;
+    if (
+      "category" in filters &&
+      filters.category !== "" &&
+      filters.category !== productObj.category
+    )
+      return null;
+
+    return new ProductCard(productObj).elem;
+  }
+
+  updateFilter(filterObject) {
+    this.#filters = filterObject;
+    this.#render();
   }
 }
