8eb0814a1a627ada0cde66e1ed9107fd6d2caa0f
Roman Lancov <skvirl@gmail.com>
2022-08-16 22:03:51 +0300

    8-4 +/- event in progress

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -5,6 +5,7 @@ import Modal from "../../7-module/2-task/index.js";
 
 export default class Cart {
   cartItems = []; // [product: {...}, count: N]
+  #modalCartWrapper = document.createElement("div");
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
@@ -19,7 +20,7 @@ export default class Cart {
     if (cartItem) {
       cartItem.count++;
     } else {
-      let cartItem = { product, count: 1 };
+      cartItem = { product, count: 1 };
       this.cartItems.push(cartItem);
     }
 
@@ -31,9 +32,7 @@ export default class Cart {
     if (!cartItem) return;
     cartItem.count += amount;
     if (cartItem.count <= 0)
-      this.cartItems = this.cartItems.filter((val) => {
-        val?.id === productId;
-      });
+      this.cartItems = this.cartItems.filter((val) => val?.product?.id !== productId);
 
     this.onProductUpdate(cartItem);
   }
@@ -117,32 +116,45 @@ export default class Cart {
   }
 
   renderModal() {
-    const modalButtonEvent = (event) => {
-      this.updateProductCount(
-        event.currentTarget.closest(".cart-product").dataset.productId,
-        event.currentTarget.matches(".cart-counter__button_plus") ? 1 : -1
-      );
-    };
-
     let modal = new Modal();
     modal.setTitle("Your order");
-    const cartWrapper = document.createElement("div");
-    modal.setBody(cartWrapper);
-    this.cartItems.forEach((element) => {
-      cartWrapper.append(this.renderProduct(element.product, element.count));
-    });
-    cartWrapper.append(this.renderOrderForm());
 
-    cartWrapper.querySelector(".cart-counter__button_plus").onclick =
-      modalButtonEvent;
-    cartWrapper.querySelector(".cart-counter__button_minus").onclick =
-      modalButtonEvent;
+    this.#modalCartWrapper.innerHTML = "";
+    modal.setBody(this.#modalCartWrapper);
+
+    this.cartItems.forEach((element) => {
+      this.#modalCartWrapper.append(
+        this.renderProduct(element.product, element.count)
+      );
+    });
+
+    this.#modalCartWrapper.append(this.renderOrderForm());
+
+    this.#modalCartWrapper.addEventListener("click", (event) => {
+      const plusButtonPushed = event.target.closest(
+        ".cart-counter__button_plus"
+      );
+      const minusButtonPushed = event.target.closest(
+        ".cart-counter__button_minus"
+      );
+
+      const currentCartProduct =
+        event.target.closest(".cart-product").dataset.productId;
+
+      if (plusButtonPushed) this.updateProductCount(currentCartProduct, 1);
+      if (minusButtonPushed) this.updateProductCount(currentCartProduct, -1);
+
+    });
 
     modal.open();
   }
 
   onProductUpdate(cartItem) {
     // ...ваш код
+    const modalCartCounter = this.#modalCartWrapper.querySelector(
+      `[data-product-id=${cartItem.product.id}] .cart-counter__count`
+    );
+    if (modalCartCounter) modalCartCounter.innerHTML = cartItem.count;
 
     this.cartIcon.update(this);
   }
