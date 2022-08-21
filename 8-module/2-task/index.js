4407689640213c8064194cd3feb4627e98557b2f
Roman Lancov <skvirl@gmail.com>
2022-08-21 19:33:12 +0300

    project assembling complete! :)

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -4,11 +4,13 @@ import ProductCard from "../../6-module/2-task/index.js";
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
-    this.filters = {};
+    this.elem = document.createElement("div");
+    this.elem.classList.add("products-grid");
     this.#render();
   }
 
   #filters = {};
+  elem = document.createElement("div");
 
   #setFilters(val) {
     for (const key in val) this.#filters[key] = val[key];
@@ -22,29 +24,24 @@ export default class ProductGrid {
   // productGrid.updateFilter({ category: '' });
 
   #render() {
-    console.clear();
-    console.log(this.#filters)
-
-    this.elem = createElement(`
-    <div class="products-grid">
-      <div class="products-grid__inner">
-        ${this.products
-          .map(this.#eligibledProduct)
-          .reduce(
-            (renderedCards, card) =>
-              (renderedCards += card === null ? "" : card.outerHTML),
-            ""
-          )}
-      </div>
-    </div>
-    `);
+    const innerGrid = document.createElement("div");
+    this.elem.replaceChildren(innerGrid);
+    innerGrid.classList.add("products-grid__inner");
+    innerGrid.append(
+      this.products
+        .map(this.#eligibledProduct)
+        .reduce((renderedCards, card) => {
+          card !== null && renderedCards.append(card);
+          return renderedCards;
+        }, new DocumentFragment())
+    );
   }
 
   #eligibledProduct = (productObj) => {
     const filters = this.#filters;
 
-    if (filters.noNuts && "nuts" in productObj) return null;
-    if (filters.vegeterianOnly && !("vegeterian" in productObj)) return null;
+    if (filters.noNuts && productObj.nuts) return null;
+    if (filters.vegeterianOnly && !productObj.vegeterian) return null;
     if (
       "maxSpiciness" in filters &&
       filters.maxSpiciness < productObj.spiciness
@@ -52,13 +49,12 @@ export default class ProductGrid {
       return null;
     if (
       "category" in filters &&
-      filters.category !== "" &&
+      filters.category &&
       filters.category !== productObj.category
     )
       return null;
-      console.log(1);
     return new ProductCard(productObj).elem;
-  }
+  };
 
   updateFilter(filterObject) {
     this.#setFilters(filterObject);
--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -1,21 +1,77 @@
-import Carousel from '../../6-module/3-task/index.js';
-import slides from '../../6-module/3-task/slides.js';
+import Carousel from "../../6-module/3-task/index.js";
+import slides from "../../6-module/3-task/slides.js";
 
-import RibbonMenu from '../../7-module/1-task/index.js';
-import categories from '../../7-module/1-task/categories.js';
+import RibbonMenu from "../../7-module/1-task/index.js";
+import categories from "../../7-module/1-task/categories.js";
 
-import StepSlider from '../../7-module/4-task/index.js';
-import ProductsGrid from '../../8-module/2-task/index.js';
+import StepSlider from "../../7-module/4-task/index.js";
+import ProductsGrid from "../../8-module/2-task/index.js";
 
-import CartIcon from '../../8-module/1-task/index.js';
-import Cart from '../../8-module/4-task/index.js';
+import CartIcon from "../../8-module/1-task/index.js";
+import Cart from "../../8-module/4-task/index.js";
 
 export default class Main {
-
-  constructor() {
-  }
+  constructor() {}
 
   async render() {
-    // ... ваш код
+    
+    const carousel = new Carousel(slides);
+    document.body.querySelector("[data-carousel-holder]").append(carousel.elem);
+
+    const ribbonMenu = new RibbonMenu(categories);
+    document
+      .querySelector("[data-ribbon-holder]")
+      .append(ribbonMenu.elem);
+
+    const stepSlider = new StepSlider({ steps: 5, value: 2 });
+    document.querySelector("[data-slider-holder]").append(stepSlider.elem);
+
+    const cartIcon = new CartIcon();
+    document.querySelector("[data-cart-icon-holder]").append(cartIcon.elem);
+    const cart = new Cart(cartIcon);
+
+    const resp = await fetch("products.json");
+    const products = resp.ok ? await resp.json() : [];
+
+    const productsGrid = new ProductsGrid(products);
+    document
+      .querySelector("[data-products-grid-holder]")
+      .replaceChildren(productsGrid.elem);
+
+    productsGrid.updateFilter({
+      noNuts: document.getElementById("nuts-checkbox").checked,
+      vegeterianOnly: document.getElementById("vegeterian-checkbox").checked,
+      maxSpiciness: stepSlider.value,
+      category: ribbonMenu.value,
+    });
+
+    document.body.addEventListener("product-add", (event) => {
+      cart.addProduct(
+        products.find((val) => val?.id === event.detail)
+      );
+    });
+
+    stepSlider.elem.addEventListener("slider-change", (event) => {
+      productsGrid.updateFilter({ maxSpiciness: event.detail });
+    });
+
+    ribbonMenu.elem.addEventListener("ribbon-select", (event) => {
+      productsGrid.updateFilter({ category: event.detail });
+    });
+
+    const nutsCheckbox = document.querySelector("#nuts-checkbox");
+    nutsCheckbox &&
+      nutsCheckbox.addEventListener("change", (event) => {
+        productsGrid.updateFilter({ noNuts: event.target.checked });
+      });
+
+    const vegeterianCheckbox = document.querySelector("#vegeterian-checkbox");
+    vegeterianCheckbox &&
+      vegeterianCheckbox.addEventListener("change", (event) => {
+        productsGrid.updateFilter({ vegeterianOnly: event.target.checked });
+      });
+
+
+
   }
 }
